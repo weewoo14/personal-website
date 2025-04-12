174b9eec566ec4c2bbf2c1208efafa51e7d42aba
weewoo14 <owenwdd01@gmail.com>
2025-04-11 20:17:44 -0400

    Added the footer + updated UI

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,24 @@
 import Home from './Home.jsx'
 import HomeCards from './HomeCards.jsx'
+import HomeFooter from './HomeFooter.jsx';
 
 function App() {
   return (
     <>
       <Home/>
+
+      <h1 className = "learnmore">
+        Learn More About Me!
+      </h1>
+
       <div className = "HomePageButtonDiv">
         <HomeCards headername = "Projects" paragraphname = "ProjectsP" title = "Projects" description = "Learn about some of the cool projects I've worked on in the past!"/>
-        <HomeCards headername = "Projects" paragraphname = "ProjectsP" title = "Projects" description = "Learn about some of the cool projects I've worked on in the past!"/>
-        <HomeCards headername = "Projects" paragraphname = "ProjectsP" title = "Projects" description = "Learn about some of the cool projects I've worked on in the past!"/>
-        
+        <HomeCards headername = "Projects" paragraphname = "ProjectsP" title = "Experience" description = "Learn about my professional experiences with others."/>
+        <HomeCards headername = "Projects" paragraphname = "ProjectsP" title = "Links" description = "Other places you can find me!"/>
       </div>
+
+      
+      <HomeFooter/>
     </>
   );
 }
---
+++ my-react-app/src/HomeFooter.jsx
@@ -0,0 +1,10 @@
+function HomeFooter() {
+  return (
+    <footer className = "HomeFooter">
+      <hr></hr>
+      <p> &copy; {new Date().getFullYear()} Haoshi (Owen) Wu </p>
+    </footer>
+  );
+}
+
+export default HomeFooter
