0f7ae46d108c54b36e7e7f66206ce20c46730ce8
weewoo14 <owenwdd01@gmail.com>
2026-03-21 11:46:46 -0400

    Start of new personal website

---
+++ components/Introduction.tsx
@@ -0,0 +1,14 @@
+function Introduction() {
+  return(
+    <div className="flex flex-col justify-center items-center bg-linear-to-b from-[#264b94] to-[#7ec0f9] h-[100vh]">
+      <h1 className="font-caveat text-white text-[20vw] md:text-[10vw]">
+        Haoshi Wu
+      </h1>
+      <p className="font-caveat text-white text-[5vw] md:text-[3vw]">
+        Aspiring CS student at [insert univeristy here]
+      </p>
+    </div>
+  );
+}
+
+export default Introduction;
